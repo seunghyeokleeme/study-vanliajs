abe2be3fa122e49f4fd69d7d48a2a144c4dab725
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-09-11 16:49:19 +0900

    #2.1 Your first JS Function

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-const devinanInfo = {
-  name: "Devinan",
-  age: 20,
-  developer: true,
-  language: ["html", "css", "javascript", "mongodb", "phyton"],
-  favStudy: [
-    {
-      name: "nomadcorder",
-      month: 3,
-      like: "clone-coding"
-    },
-    {
-      name: "생활코딩",
-      month: 5,
-      like: "easy-talk"
-    }
-  ]
-};
+function sayHello(name, age) {
+  console.log("Hello", name, " you have ", age, "years old");
+}
 
-console.log(devinanInfo.favStudy[0].month);
+sayHello("Devinan", 20);
 
-/* 
-{} 는 object 이다.
-{}는 array와 다르게 데이터에게 라벨을 붙인다. 즉 db에서 데이터리스트를 합치고 정보를 만들때 array보다 {}를 쓰는게 맞다.
-그리고 object은 array 안에도 들어갈수 있다.
+/* function 형태
+ name, age 는 argument(인자) = 변수 
+ console.log(string) ==> 기록 
 */
