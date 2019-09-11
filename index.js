c79b9b7a714d04facae84b8f613b368182448b1a
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-09-11 14:53:00 +0900

    #1.6 Your first JS Variable

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-alert("Im devinan");
+let a = 221;
+let b = a - 5;
+a = 4;
+console.log(b, a);
 
-// This is a helloworld
+/* javascript는 위에서 아래로 읽는다.
+생성 초기화 사용 이러한 3방식의 흐름으로 진행한다.
+let은 변할수 있는 상수여서 고정상수가 아니다.
+그리고 항상 라인의 끝에는 ; 를 붙인다.
+*/
