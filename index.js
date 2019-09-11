c4e690f0809de8c6ad6098161c630b4762519066
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-09-11 22:38:53 +0900

    #2.3 Modifying the DOM with JS

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const title = document.getElementById("title");
 
 title.innerHTML = "Hi! From JS";
 
-// DOM
+title.style.color = "red";
+
+document.title = "I own you now";
+
+// console.dir() 은 객체의 프로퍼티들의 가능성을 보는 것
