ef8287a8ddcd5600dfdb9d9e3a01e857770a64e4
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-09-11 17:13:06 +0900

    #2.1.1 More Function Fun

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
 function sayHello(name, age) {
-  console.log("Hello", name, " you have ", age, "years old");
+  return `Hello ${name} you have ${age} years old`;
 }
 
-sayHello("Devinan", 20);
+const greetingDevinan = sayHello("Devinan", 20);
 
-/* function 형태
- name, age 는 argument(인자) = 변수 
- console.log(string) ==> 기록 
+console.log(greetingDevinan);
+
+/* 
+    greetingDevinan 은 sayHello함수를 실행한 결과값이다 = 즉 리턴한값이다.
+    만약 함수안에 console.log가 되어 있으면 성립되지 않아 undefind가 된다.
 */
+
+const calculator = {
+  plus: function(a, b) {
+    return a + b;
+  },
+  minus: function(a, b) {
+    return a - b;
+  },
+  divide: function(a, b) {
+    return a / b;
+  },
+  multiple: function(a, b) {
+    return a * b;
+  },
+  power: function(a, b) {
+    return a ** b;
+  }
+};
+
+const a = 5;
+const b = 5;
+
+const plus = calculator.plus(a, b);
+const minus = calculator.minus(a, b);
+const divide = calculator.divide(a, b);
+const multiple = calculator.multiple(a, b);
+const power = calculator.power(a, b);
+
+console.log(plus);
+console.log(minus);
+console.log(divide);
+console.log(multiple);
+console.log(power);
+
+// 실습 계산기
