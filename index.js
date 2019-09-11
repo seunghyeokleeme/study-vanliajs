84edf6eb332a97ff58aa705359db219becd12be5
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-09-11 15:36:10 +0900

    #1.8 Data Types on JS

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const a = 221;
-const b = a - 5;
+/* String
+const what = "devinan";
+ String 은 문자열
+*/
 
-console.log(b, a);
+/* Boolean 
+const wat = true;
+    Boolean 은 참(1) 거짓(0)
+*/
 
-/* let, const, var 은 각각 변수이다.
-    let 은 변수가 변할수 있다.
-    const 은 상수이다. (변할수 없는 고정값) 현재 이변수를 가장 많이 쓰인다.
-    >> Why??  -- 오류를 쉽게 찾을 수있기 때문이다.
-    var 은 3년전까지 이 변수만 쓰이게 되었다.
+/* Number 
+const wat = 123;
+    Number 은 숫자
+*/
+
+/* Float
+const wat = 123.435;
+console.log(wat);
+    Float 은 소수자리까지
 */
