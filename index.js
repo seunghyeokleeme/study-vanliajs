a78160dc0e136bcfc1cf3a54136633283d399ee6
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-09-12 01:37:30 +0900

    #2.4 Events and event handlers

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const title = document.querySelector("#title");
 
-title.innerHTML = "Hi! From JS";
+function handleClick() {
+  title.style.color = "red";
+}
 
-title.style.color = "red";
-
-document.title = "I own you now";
-
-// console.dir() 은 객체의 프로퍼티들의 가능성을 보는 것
+window.addEventListener("click", handleClick);
